d2bf0d1ee8d13f20a07bf0309e3a37b8ea17f6b2
Krushnakhaire <krushnakhaire2004@gmail.com>
2025-09-12 15:14:36 +0530

    update

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -5,12 +5,12 @@ import { assets } from '../../assets/assets'
 const Footer = () => {
   return (
     <div className='footer' id='footer'>
-    <div className='foter-contet'>
+    <div className='footer-content'>
         <div className='footer-item-left'>
             <img src={assets.logo} alt=""/>
             <p></p>
 
-            <div className='footer social-icons'>
+            <div className='footer-social icons'>
                 <img src={assets.facebook_icon} alt=''/>
                 <img src={assets.instagram_icon} alt=""/>
                 <img src={assets.twitter_icon} alt=''/>
@@ -18,13 +18,26 @@ const Footer = () => {
             </div>
     </div>
     <div className='footer-content-center'>
+      <h2>COMPANY</h2>
+      <ul>
+        <li>Home</li>
+        <li>About</li>
+        <li>Delivery</li>
+        <li>Privacy policy</li>
+      </ul>
         </div>
         <div className='footer-content-right'>
+          <h2>GET IN TOUCH</h2>
+          <ul>
+            <li>123456789</li>
+            <li>contact@gmail.com</li>
+
+          </ul>
 
         </div>
-
-
     </div>
+    <hr/>
+    <p className='footer-copyright'>Copyright 2024 @ tomato.com  -  All Right Reserved</p>
     </div>
   )
 }
