adbfee7a8cea2d28410bc036ee052c8b393ecf96
Krushnakhaire <krushnakhaire2004@gmail.com>
2025-09-29 21:54:11 +0530

    update

--- src/pages/PlaceOrder/PlaceOrder.jsx
+++ src/pages/PlaceOrder/PlaceOrder.jsx
@@ -1,9 +1,32 @@
 import React from 'react'
 import './PlaceOrder.css'
 
- const PlaceOrder = () => {
+const PlaceOrder = () => {
   return (
-    <div>PlaceOrder</div>
+    <form className='place-order'>
+      <div className='place-order-left'>
+        <p className='title'>Delivery Information</p>
+        <div className='multi-fields'>
+          <input type="text" placeholder='First name' />
+          <input type="text" placeholder='Last name' />
+        </div>
+        <input type="email" placeholder='Email Address' />
+        <input type="text" placeholder='Street' />
+        <div className='multi-fields'>
+          <input type="text" placeholder='City' />
+          <input type="text" placeholder='State' />
+        </div>
+        <div className='multi-fields'>
+          <input type="text" placeholder='Zip code' />
+          <input type="text" placeholder='Country' />
+        </div>
+        <input type='text' placeholder='Phone' />
+      </div>
+      <div className='place-order-right'>
+        {/* Add order summary or payment section here */}
+      </div>
+    </form>
   )
 }
+
 export default PlaceOrder
