b4c907975d164f18f0febb64a23c5e042d60e52d
Krushnakhaire <krushnakhaire2004@gmail.com>
2025-10-09 23:23:10 +0530

    Update backend: add foodController and other changes

---
+++ backend/controllers/foodController.js
@@ -0,0 +1,16 @@
+import foodModel from "../models/foodModel.js";
+
+import fs from "fs";
+
+//add food item
+
+const addFood=async(req,res)=>{
+
+    let image_filename=`${req.file.image_filename}`;
+
+    const food =new foodModel({
+        
+    })
+
+}
+export{addFood}
--- backend/routes/foodRoute.js
+++ backend/routes/foodRoute.js
@@ -4,7 +4,18 @@ import multer from "multer";
 
 const foodRouter=express.Router();
 
+const storage=multer.diskStorage({
+    destination:"uploads",
+    filename:(req,file,cb)=>{
+        return cb(null,`${Date.now()}${file.originalname}`)
+    }
+})
+
+const upload=multer({storage:storage})
+
 foodRouter.post("/add/food",addFood)
+
+
 
 
 export default  foodRouter;
